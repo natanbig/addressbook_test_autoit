041df73fead50d3444b0e9d631f569afeb835d10
Natan Radostin <natanbig@gmail.com>
2017-11-12 16:12:24 +0200

    New project created
    Emty main class

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_tests_autoit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
